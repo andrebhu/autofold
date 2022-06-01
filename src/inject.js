cd30f3815793a010c3f8312d898905379b1ffe36
andrebhu <andrebhu@gmail.com>
2022-06-01 12:20:08 -0400

    Properly read cards from obsevers

--- src/inject.js
+++ src/inject.js
@@ -1,16 +1,40 @@
 console.log("inject.js!");
 
+class Card {
+    constructor(value, suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+}
+
 const player = document.getElementsByClassName("you-player")[0];
-const playerHand = player.querySelector(".table-player-cards");
+const card1 = player.getElementsByClassName("card-container")[0];
+const card2 = player.getElementsByClassName("card-container")[1];
+
+
+
 
 let observer = new MutationObserver(mutationRecords => {
-    // console.log(mutationRecords);
+    var cards = [];
     for (let record of mutationRecords) {
-        console.log(record["target"]);
+        let e = record["target"];
+
+        let value = e.getElementsByClassName("value")[0].innerHTML;
+        let suit = e.getElementsByClassName("suit")[0].innerHTML;
+        cards.push(new Card(value, suit));
     }
+
+    console.log(cards);
 });
 
-observer.observe(playerHand, {
+
+// create observers
+observer.observe(card1, {
+    attributes: true,
+    childList: true,
+    subtree: true
+});
+observer.observe(card2, {
     attributes: true,
     childList: true,
     subtree: true
